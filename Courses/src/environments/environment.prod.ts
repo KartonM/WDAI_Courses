1c8cdc1802bdd7e4f96f0d3cb18ccc67a73cfbff
Marcin Kozubek <mkozubek@protonmail.com>
2020-01-22 15:58:25 +0100

    Added firebase api setup to production

--- Courses/src/environments/environment.prod.ts
+++ Courses/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyByr_sHiSUgTnKKIdkTpLxAjRDZFlRhYS4",
+    authDomain: "wdai-courses.firebaseapp.com",
+    databaseURL: "https://wdai-courses.firebaseio.com",
+    projectId: "wdai-courses",
+    storageBucket: "wdai-courses.appspot.com",
+    messagingSenderId: "463946576158",
+    appId: "1:463946576158:web:8d288c8c6d0be2b4b9590f"
+  }
 };
